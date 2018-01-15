a440b3c4871c804a1a664a960104ac0b7c8a1ed5
161710089 <161710089@smkassalaambandung.sch.id>
2018-01-15 08:34:12 +0700

    Layouts

---
+++ resources/views/konten2.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.master2')
+@section('konten2')
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<CENTER>    
+       <img src="c.jpg"  style="width: 500px; height: 300px" alt="Generic placeholder thumbnail"><br>
+</CENTER>
+  
+<br><br>
+<center> <h3> <span class="label label-default">Prestasi Pelatih Persib Dari Masa Ke Masa</span></h3></center>
+
+1.Omo Suratmo <br>
+persib menjadi runner up pada Kompetesi Perserikatan 1982-1983 setelah di final dikalahkan PSMS melalui
+drama adu penalti 2-3(0-0).<br> <br>
+2.Ade Dana<br>
+Arsitek Persib saat meraih juara Kompetensi Perserikatan 1989-1990 dan menjadi runner-up Kompetensi 1984-1985 
+setelah dikalahkan PSMS 3-4(2-2)melalui adu penalti<br><br>
+3.Nandar Iskandar<br>
+Membawa Persib juara Kompetensi Perserikatan 1986. Ia Kembai di percaya menjadi pelatih persin selama du musim pada kompetensi 1996-1997 dan 1997-1998 <br><br>
+4.Indra M. Tohir<br>
+Mengantarkan Persib menjuarai kompetensi Perserikatan 1994 dan Liga Indonesia I 1994-1995. Ia kembali 
+di percaya pada kompetensi 2001-2002 dan 2005-2006.<br><br>
+5.Risnandar Soendoro<br>
+Meloloskan Persib ke babak"12 Besar"Liga Indonesia II/1995-1996 dan kembali menjadi pelatih pada kompetensi 
+2006-2007,tetapi mundur di putaran pertama.<br><br>
+6.M.Suryamin <br>
+Menjadi pelatih pada kompetisi 1998-1999. kemudian dipertahankan setahun,tetapi mundur pada putaran pertama. era
+dia mulai menggunakan pemain-pemain muda.<br><br>
+7.Deny Syamsudin<br>
+persib menepati peringkat 8 dari 12 kontestan pada kompetisi 2001-2002. kompetisi sebelumnya, mengeban tugas sebagai 
+pelatih asisten pelatih Indra Tohir<br><br>
+8.Arcan Andrejz Sledzianowski<br>
+Menagani persib pada kompetisi IX/2003. karena prestasi tim buruk pada awal kompetisi, pelatih asal polandia ini langsung diberhentikan dan digantikan oleh Juan Paez<br><br>
+9.Juan Antoni Paez<br>
+Menjadi penyelamat persib dari degradasi pada kompetisi 2003.Musim berikutnya 2004-2005,pelatih asal cile ini membawa pesib naik ke peringkat 6<br><br>
+10.Arcan Luri Anatolivieci<br>
+Pelatih asal Moldova ini, menjadi pelatih pada kompetisi 2006-2007 menggatikan Risnandar.Musim berikutnya, masih mejadi pelantih dan menepati peringkat 5<br><br>
+11.Jaya Hartono<br>
+Persib Menempatai Peringkat 3 kompetisi 2008-2009. Kemudian,diperpanjang Setahun,tetap mengundurkan diri pada pertengahan putaran kedua.<br><br>
+12.Daniel Darko Janackovic
+Daniel menjadi pelatih tercepat yang diganti.Sebelum komptensi 2010-2011 bergulir,pelatih asal Serbia ini sudah diganti oleh rekannya,jovo Cuckovic.<br><br>
+13.joko Cuckovic <br>
+Pria asal Serbia ini menjadi pelatih musim kompetensi 2010-2011 setelah menggantikan Daniel Darko Janakovic.Karena Kendala bahasa,dia menjadi Penasihat Teknik<br><br>
+14.Daniel Roekito<br>
+Menjadi pelatih ketiga Persib pada musim kompetensi 2010-2011.Ia menggantikan Jovo Cuckovic setelah"Maung Bandung"menyelesaikan enam pertandingan.<br><br>
+15.Djajang Nurjaman<br>
+<p>Sebagai pelatih, Djadjang merasakan gelar juara ketika menjadi asisten pelatih Indra M. Thohir di Liga Indonesia (LI) I/1994-1995 dan masih dipercaya hingga tahun 1996. Setelah itu ia lebih memantapkan karier kepelatihan dengan menukangi PERSIB Junior (U-23). Pada tahun 2006 lagi-lagi ia mendapat kepercayaan sebagai asisten pelatih untuk mendampingi Arcan Iurie. Setelah itu ia mengembangkan karier kepelatihan di luar PERSIB, hingga pada tahun 2012, manajemen PERSIB mempercayakan dirinya untuk menukangi tim sebagai Pelatih Kepala dalam mengarungi Indonesia Super League tahun 2013.</p>
+
+<p>Kembalinya ke PERSIB seolah mengulang romantisme juara dengan rekan-rekannya di Liga Indonesia I. Namun kali ini ia menjadi pelatih kepala, "abah" Indra Thohir sebagai Direktur Teknik, dan juga ia dibantu oleh trio mantan pemain yang mengantarkan PERSIB juara LI I, yaitu Anwar Sanusi, Asep Soemantri, dan Sutiono Lamso sebagai asisten pelatih. Hasilnya tidak mengecewakan, di ajang turnamen pra musim Celebes Cup yang digelar di kota Bandung, Djadjang mempersembahkan tropi juara setelah di final mengalahkan Sriwijaya FC dengan skor 1-0.</p>
+
+<p> Musim selanjutnya (2014) Djadjang masih didaulat sebagai pelatih kepala, kali ini ia mengajak Herrie Setiawan, Asep Soemantri dan Anwar Sanusi sebagai asisten pelatih.Pada Musim ini,Djadjang Nurdjaman berhasil membawa Persib menjadi juara isl 2014.Djadjang juga mencetak sebuah rekor,yaitu mengantarkan Persib menjadi juara liga sebagai pemain,asisten pelatih, dan pelatih kepala</p>
+
+<p>Musim 2015 Djajang menjalani musim ketiga bersama Persib. Di QNBL 2015 Djajang berhasil meraih 2 kemenangan dari 2 pertandingan sebelum liga dihentikan karena PSSI dibekukan oleh FIFA. Di kompetisi AFC Cup Djajang berhasil membawa Persib lolos ke babak 16 besar,Di AFC cup langkah persib terhenti di 16 besar karena kalah 0-2 dari Hanoi T&T .</p>
+
+<p>Satu tropi kembali Djajang sumbangkan untuk Persib pada tahun 2015. Persib dipimpinnya menjadi juara Piala Presiden 2015. setelah 20 tahun akhirnya Djajang kembali ke Stadion GBK, sebagai pelatih Djajang berhasil meraih gelar Juara mengalahkan Sriwijaya FC 2-0.</p>
+
+<p>Akhirnya, pada tanggal 15 Juli 2017 Djajang resmi mengundurkan diri sebagai pelatih kepala Persib Bandung dikarena kecewa tidak bisa mendongkrak penampilan tim berjuluk "Maung Bandung" hingga menyelesaikan laga ke-15 kompetisi Liga 1 2017.</p> 
+
+
+</body>
+</html>
+@endsection
---
+++ resources/views/konten3.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.master3')
+@section('konten3')
+
+Liga Indonesia 2005<br>
+Chioma Kingsley (Nigeria)<br>
+Antonio Claudio (Brasil)<br>
+Nipont Chanarwut (Thailand)<br>
+Pradith Taweechai (Thailand)<br>
+<br>
+Liga Indonesia 2006<br>
+Ayouck Berti (Kamerun)<br>
+Patricio Jimenez (Cile)<br>
+<br>
+Liga Indonesia 2005-2006<br>
+Ulian de Souza (Brasil)<br>
+Kosin Hatharaitanakool (Thailand)<br>
+<br>
+Liga Indonesia 2006 dan 2009<br>
+Redouane Barkaoui (Maroko)<br>
+<br>
+Liga Indonesia 2006-2008<br>
+Brahima Traore (Burkina Faso)<br>
+<br>
+Liga Indonesia 2007-2008<br>
+Nyeck Nyobe (Kamerun)<br>
+Lorenzo Cabanas (Paraguay)<br>
+Christian Bekamenga (Kamerun)<br>
+Leontin Chitescu (Rumania)<br>
+<br>
+Liga Super Indonesia 2008-2009<br>
+Fabio Lopez Alcantara (Brasil)<br>
+Rafael Alves Bastos (Brasil)<br>
+Satoshi Otomo (Jepang)<br>
+Christian Rene Martinez (Paraguay)<br>
+<br>
+Liga Super Indonesia 2009-2010<br>
+Suchao Nuchnum (Thailand)<br>
+Hilton Moreira (Brasil)<br>
+Christian Gonzalez (Uruguay)<br>
+<br>
+Liga Super Indonesia 2010-2011<br>
+Pablo Frances (Argentina)<br>
+Baihakki Khaizan (Singapura)<br>
+Shahril Ishak (Singapura)<br>
+Abanda Herman (Kamerun).<br>
+<br>
+Liga Super Indonesia 2011 -2012<br>
+Shohei Matsunaga (Jepang)<br>
+Abanda Herman (Kamerun)<br>
+Miljan Radovic (Montenegro)<br>
+Zdravko Dragicevic (Montenegro)<br>
+Moses Sakyi (Ghana)<br>
+<br>
+Liga Super Indonesia 2012-2013<br>
+Naser Al Sebai (Suriah)<br>
+Mbida Messi (Kamerun)<br>
+Herman Dzumafo Epandi (Kamerun)<br>
+Kenji Adachihara (Jepang)<br>
+Sergio van Dijk (Belanda /akhirnya naturalisasi)<br>
+<br>
+Liga Indonesia 2014<br>
+Vladimir Vujovic (Montenegro)<br>
+Djibril Coulibaly (Mali)<br>
+Makan Konate (Mali)<br>
+<br>
+Liga 1 2017<br>
+Michael Essien (Ghana)<br>
+Carlton Cole (Inggris)<br>
+Vladimir Vujovic (Montenegro)<br>
+Shohei Matsunaga (Jepang)<br>
+Ezechiel N’Douassel (Chad)<br>
+@endsection
